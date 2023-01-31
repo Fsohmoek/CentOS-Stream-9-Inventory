f8219e49a727953c3e428bc881e29162817eed83
Fsohmoek <sohmoekamtefranki@gmail.com>
2023-01-31 13:27:27 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+CentOS-Stream-9-Inventory
+This bash script works on linux CentOS Stream 9
+The script audit the server
---
+++ script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+    #Author : Franki
+    #Date : 31-Jan-2023
+
+## ---------- This script is to inventory a Linux CentOS Stream 9 Server -----------------
+# Checking the OS version
+
+echo " The OS version is $(cat /etc/*release | grep PRETTY_NAME | awk -F = '{print $2}')"
+
+# Checking the memory size
+
+echo " The memory size is $(free -mh |grep Mem: |awk '{print $2}')"
+
+# Checking the hard drive
+
+echo " The hard drive is $(lsblk |grep sda)"
+
+# Checking the CPU speed
+
+echo " The CPU speed is $(lscpu |grep .Hz)"
+
+# Checking the kernel version
+
+echo " The kernel version is $(uname -r |awk -F"x" '{print $1}')"
+
+# Checking if the system is 32 or 64 bits
+
+echo " The system is $(getconf LONG_BIT) bits"
+
+# Checking the server's hostname
+
+echo "The server's hostname is $(hostname)"
+
+# Checking the IP address of the server
+
+echo "The IP address of the server is $(hostname -I | awk '{print $1}')"
+
+# Checking the DNS of the server
+
+echo "The DNS of the server is $(dig | grep SERVER: | awk -F# '{print $1}' | awk -F: '{print $2}')"
+
+# Checking the system's manufacturer
+
+echo "The system's manufacturer is $(dmidecode -t system |grep Manufacturer: |awk -F":" '{print $2}')"
+
+# Checking whether the system is virtual or physical
+
+echo "The system is $(lshw -c system |grep product: |awk -F" " '{print$2}')"
+
+# Checking the MAC address of the system
+
+echo "The MAC address is $(ip addr sh enp0s3 |grep link/ether |awk -F" " '{print $2}')"
+
+# Checking all opened ports on the system
+
+echo "All opened ports on the system are $(lsof -i -P -n | grep LISTEN)"
+
+# Check your public IP address
+echo "Your public IP Address is $(curl -s ifconfig.co)"
+
+exit 0
